0dcaac6f101888b48505a652647c49df25f22248
zarezadehmehrzad-pixel <zarezadehmehrzad@gmail.com>
2026-06-17 22:13:21 +0400

    hello

---
+++ src/components/Newtest.jsx
@@ -0,0 +1,12 @@
+function Newrest(){
+    return(
+        <form className="form" action="">
+            <input type="text"  placeholder="name ..." name="name"/>
+            <input type="number" name="price" id="" placeholder="price" />
+            <textarea name="details" id=""></textarea>
+            <button>add</button>
+        </form>
+    )
+}
+
+export default Newtest
